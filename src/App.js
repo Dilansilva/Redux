b157297fe698060b9bbdaa4a72773e71195d0ad2
Dilan Silva <sdilansilva@gmail.com>
2021-05-15 22:59:28 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import TodoList from "../src/components/TodoList";
 
+import {useSelector,useDispatch} from 'react-redux';
+import {increment,decrement} from './actions'
+
 function App() {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch();
   return (
     <div>
-      <TodoList/>
+      <h1>Counter : {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 
+import allReducers from "./reducers";
+import { createStore } from "redux";
+import {Provider} from 'react-redux';
+
+const myStore = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Provider store={myStore}>
+  <App />
+  </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
