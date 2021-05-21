f28036331faf70646861badd7f2a82755863828d
Dilan Silva <sdilansilva@gmail.com>
2021-05-22 00:07:07 +0530

    index.js is creatde and combined the reducers

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import addTask from './addTask';
+import IncrementDecrement, { Increment } from './IncrementDecrement';
+import {combineReducers} from 'redux';
+
+const allReducers = combineReducers({
+    Create : addTask,
+    Counter : IncrementDecrement
+});
